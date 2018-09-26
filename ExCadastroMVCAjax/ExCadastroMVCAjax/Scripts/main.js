1197be36c0c87896172eecc0e6655e037b909c4e
Gabriel Fernandes Medeiros <gblmedeiros@gmail.com>
2018-09-26 10:20:35 -0300

    Add files via upload

--- ExCadastroMVCAjax/ExCadastroMVCAjax/Scripts/main.js
+++ ExCadastroMVCAjax/ExCadastroMVCAjax/Scripts/main.js
@@ -1,86 +1,217 @@
 ﻿$(function () {
     $('span').css('color', 'red');
+    ListaPaciente();
     $('#btnCadastrar').click(function () {
-        var nome = $('#txtNome').val();
-        var idade = $('#txtIdade').val();
-        var sexoM = $('#Masc').is(':checked');
-        var sexoF = $('#Fem').is(':checked');
-        var tel = $('#txtTel').val();
-        var cel = $('#txtCel').val()
-        var resp = $('#txtResp').val();
-
-        var erro = "";
-        $('#errors').html('');
-
-        if (nome == "")
-            erro += "<p>* Campo nome é Obrigatório </p>";
-        if (idade == "")
-            erro += "<p>* Campo idade é Obrigatório </p>";
-        if (!sexoM && !sexoF)
-            erro += "<p>* Favor informar o Sexo </p>";
-        if (tel == "")
-            erro += "<p>* Campo telefone é Obrigatório </p>";
-        if (resp == "" && idade < 18)
-            erro += "<p>* Necessário informar responsável para menor de idade! </p>";
-
-        if (erro) {
-            $('#errors').html(erro);
-            return false;
-        } else {
-            var paciente = {};
-            paciente.nome = $('#txtNome').val();
-            paciente.idade = $('#txtIdade').val();
-            paciente.sexo = $("input[name='Sexo']:checked").val();
-            paciente.telefone = $('#txtTel').val();
-            paciente.celular = $('#txtCel').val();
-            paciente.email = $('#txtEmail').val();
-            paciente.responsavel = $('#txtResp').val();
-
-            $.ajax({
-                url: "/Create/Insert",
-                data: JSON.stringify(paciente),
-                type: "POST",
-                contentType: "application/json; charset=utf-8",
-                dataType: "json",
-                success: function (result) {
-                    $('#msg').html("Paciente Cadastrado Com Sucesso!");
-                    $('#CriaPaciente').trigger("reset");
-                },
-                error: function (errormensage) {
-                    alert(errormessage.responseText);
-                }
-            });
-        };
-        return true;
+        CriaPaciente();
     });
+    $('#btnAlterar').click(function () {
+        AlteraPaciente();
+    });
+    $('#CancelarEdicao').click(function () {
+        $('#ModalEditar').modal('hide');
+    });
+});
 
-    $(function ListaPaciente()
-    {
+function CriaPaciente() {
+    var res = ValidaFormulario();
+    if (res == false) {
+        $('#error').html("Campo(s) Obrigatórios!");
+        return false;
+    }
+        var paciente = {};
+        paciente.nome = $('#nome').val();
+        paciente.idade = $('#idade').val();
+        paciente.sexo = $("input[name='sexo']:checked").val();
+        paciente.telefone = $('#tel').val();
+        paciente.celular = $('#cel').val();
+        paciente.email = $('#email').val();
+        paciente.responsavel = $('#resp').val();
+
         $.ajax({
-            url: "/List/ListAll",
-            type: "GET",
+            url: "/Create/Insert",
+            data: JSON.stringify(paciente),
+            type: "POST",
             contentType: "application/json; charset=utf-8",
             dataType: "json",
             success: function (result) {
-                var html = '';
-                $.each(result, function (key, item) {
-                    html += '<tr>';
-                    html += '<td>' + item.id + '</td>';
-                    html += '<td>' + item.nome + '</td>';
-                    html += '<td>' + item.idade + '</td>';
-                    html += '<td>' + item.sexo + '</td>';
-                    html += '<td>' + item.telefone + '</td>';
-                    html += '<td>' + item.celular + '</td>';
-                    html += '<td>' + item.email + '</td>';
-                    html += '<td>' + item.responsavel + '</td>';
-                    html += '<td><a href="#" onclick="return getbyID(' + item.id + ')">Edit</a> | <a href="#" onclick="Delele(' + item.id + ')">Delete</a></td>';
-                    html += '</tr>';
-                });
-                $('.tbody').html(html);
+                $('#msg').html("Paciente Cadastrado Com Sucesso!");
+                $('#CriaPaciente').trigger("reset");
+                $('#nome').css('border-color', 'lightgrey');
+                $('#idade').css('border-color', 'lightgrey');
+                $('#sexo').html('');
+                $('#tel').css('border-color', 'lightgrey');
+                $('#email').css('border-color', 'lightgrey');
+                $('#txtResp').css('border-color', 'lightgrey');
+                $('#error').html('');
             },
-            error: function (errormessage) {
-                alert("Erro Na listagem de Pacientes!");
+            error: function (errormensage) {
+                alert(errormessage.responseText);
             }
         });
+}
+
+function ListaPaciente() {
+    $.ajax({
+        url: "/List/ListAll",
+        type: "GET",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            var html = '';
+            $.each(result, function (key, item) {
+                html += '<tr>';
+                html += '<td>' + item.id + '</td>';
+                html += '<td>' + item.nome + '</td>';
+                html += '<td>' + item.idade + '</td>';
+                html += '<td>' + item.sexo + '</td>';
+                html += '<td>' + item.telefone + '</td>';
+                html += '<td>' + item.celular + '</td>';
+                html += '<td>' + item.email + '</td>';
+                html += '<td>' + item.responsavel + '</td>';
+                html += '<td><button type="button" id="btnEditar" data-toggle="modal" data-target="#ModalEditar" class="btn btn-info btn-sm" onclick="getbyID(' + item.id + ')">Editar</button> | <button type="button" class="btn btn-danger btn-sm" onclick="ExcluirPaciente(' + item.id + ')">Deletar</button></td>';
+                html += '</tr>';
+            });
+            $('.tbody').html(html);
+        },
+        error: function (errormessage) {
+            alert("Erro Na listagem de Pacientes!");
+        }
     });
-});
+    return false;
+}
+
+
+function getbyID(id) {
+    $('#nome').css('border-color', 'lightgrey');
+    $('#idade').css('border-color', 'lightgrey');
+    $('#sexo').html('');
+    $('#tel').css('border-color', 'lightgrey');
+    $('#email').css('border-color', 'lightgrey');
+    $('#txtResp').css('border-color', 'lightgrey');
+    $('#error').html('');
+    $.ajax({
+        url: "/List/getbyID/" + id,
+        type: "GET",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            $('#id').val(result.id);
+            $('#nome').val(result.nome);
+            $('#idade').val(result.idade);
+            result.sexo == "Masculino" ? $('#masculino').prop("checked", true) : $('#feminino').prop("checked", true);
+            $('#tel').val(result.telefone);
+            $('#cel').val(result.celular);
+            $('#email').val(result.email);
+            $('#resp').val(result.responsavel)
+        },
+        error: function (errormessage) {
+            alert("erro");
+        }
+    });
+
+}
+
+function AlteraPaciente() {
+    var res = ValidaFormulario();
+    if (res == false) {
+        $('#error').html("Campo(s) Obrigatórios!");
+        return false;
+    }
+    var paciente = {};
+    paciente.id = $('#id').val();
+    paciente.nome = $('#nome').val();
+    paciente.idade = $('#idade').val();
+    paciente.sexo = $("input[name='sexo']:checked").val();
+    paciente.telefone = $('#tel').val();
+    paciente.celular = $('#cel').val();
+    paciente.email = $('#email').val();
+    paciente.responsavel = $('#resp').val();
+
+    $.ajax({
+        url: "/List/AlteraPaciente",
+        type: "POST",
+        data: JSON.stringify(paciente),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            ListaPaciente();
+            $('#ModalEditar').modal('hide');
+            $('#AlteraPaciente').trigger("reset");
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
+
+function ExcluirPaciente(ID) {
+    var wrg = confirm("Deseja Realmente excluir o Paciente?");
+    if (wrg) {
+        $.ajax({
+            url: "/List/ExcluiPaciente/" + ID,
+            type: "POST",
+            contentType: "application/json;charset=UTF-8",
+            dataType: "json",
+            success: function (result) {
+                ListaPaciente();
+            },
+            error: function (errormessage) {
+                alert(errormessage.responseText);
+            }
+        });
+    }
+}
+//Função Para Validar o Formulário
+function ValidaFormulario() {
+    var sexoM = $('#masculino').is(':checked');
+    var sexoF = $('#feminino').is(':checked');
+
+    var valido = true;
+
+    if ($('#nome').val() == "") {
+        $('#nome').css('border-color', 'red');
+        valido = false;
+    }
+    else {
+        $('#nome').css('border-color', 'lightgrey');
+    }
+
+    if ($('#idade').val() == "") {
+        $('#idade').css('border-color', 'red');
+        valido = false;
+    }
+    else {
+        $('#idade').css('border-color', 'lightgrey');
+    }
+
+    if (!sexoM && !sexoF) {
+        $('#sexo').html('Sexo é Obrigatório!');
+        valido = false;
+    }
+
+    if ($('#tel').val() == "") {
+        $('#tel').css('border-color', 'red');
+        valido = false;
+    }
+    else {
+        $('#tel').css('border-color', 'lightgrey');
+    }
+
+    if ($('#email').val() == "") {
+        $('#email').css('border-color', 'red');
+        valido = false;
+    }
+    else {
+        $('#email').css('border-color', 'lightgrey');
+    }
+
+    if ($('#resp').val() == "" && $('#idade').val() < 18) {
+        $('#resp').css('border-color', 'red');
+        valido = false;
+    }
+    else {
+        $('#resp').css('border-color', 'lightgrey');
+    }
+
+    return valido;
+}
